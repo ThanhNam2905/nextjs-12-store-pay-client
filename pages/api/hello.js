58876087086e8a798ff5233f5ef68a053f6ff860
nam <you@example.com>
2023-07-04 15:42:12 +0700

    Setup source code and functionlaty Connect Database to MongoDB

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import db from "../../utils/database"
+
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    db.connectDB();
+    db.disconnectDB();
+    res.status(200).json({ name: 'John Doe' })
 }
